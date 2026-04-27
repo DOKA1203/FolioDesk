a7efaf6452c19cf51391576c1cf1a56c5f11fc1c
doka1203 <pomaseminecraft@gmail.com>
2026-04-27 15:59:57 +0900

    업데이트ㅡㅡㅡ

--- FolioDesk/MainWindow.xaml.cs
+++ FolioDesk/MainWindow.xaml.cs
@@ -9,6 +9,15 @@ public partial class MainWindow : Window {
         InitializeComponent();
     }
     
+    private void TitleBar_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+        if (e.ButtonState == System.Windows.Input.MouseButtonState.Pressed)
+            DragMove();
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e) {
+        Application.Current.Shutdown();
+    }
+
     private void CreateFolder(object sender, RoutedEventArgs e) {
         var folder = App.DataManager.CreateFolioFolder("New Folder");
         ShortCutManager.CreateShortcut(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "FolioDesk.exe"), folder.Id, $"New AppFolder {folder.Id}");
